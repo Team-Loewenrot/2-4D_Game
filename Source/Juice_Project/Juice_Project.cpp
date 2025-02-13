6430d489bcebdcca7f1c12841ffb14cc12b9a444
Maximilian Zhu <maximilian.zhu@loewenrot.de>
2025-02-13 17:45:23 +0100

    V8

---
+++ Source/Juice_Project/Juice_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Juice_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Juice_Project, "Juice_Project" );
